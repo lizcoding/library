db96df6e817b2245bf7a2ac11bcc29c0a7d04ee2
lizcoding <elizarn96@gmail.com>
2021-10-23 16:40:44 -0700

    Modify index.html, script.js, and style.css

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 /* 
 
 --- TO DO ---
-1. Allow user to change entry status
-2. Save library to local storage
-
+1. Save library to local storage
+2. Allow user to edit title, author, and pages.
+3. Make sure all delete (remove) buttons have different id's 
+    or change id to class name.
 */
 let myLibrary = [];
 let libraryDisplay = document.getElementById('current-library');
@@ -26,9 +27,8 @@ function Book(title, author, pages, status, index, id) {
 
 function addBookToDisplay() {
     let last = myLibrary[myLibrary.length - 1];
-    console.log(last);
     for (let property in last) {
-        if (property != 'index' && property != 'bookId') {
+        if (property == 'title' || property == 'author' ||property == 'pages') {
             let newGridItem = document.createElement("p");
             let itemContent = document.createTextNode(last[property]);
             newGridItem.appendChild(itemContent);
@@ -36,24 +36,97 @@ function addBookToDisplay() {
             libraryDisplay.appendChild(newGridItem);
         }
     }
+    addStatusDropdown(last);
+    addDeleteButton(last);
+}
+
+// ADD function adjustBookIndices() {}
+
+function addDeleteButton(book) {
+    // Enable Delete Button
     let newDeleteButton = document.createElement("button");
-    let itemContent = document.createTextNode("Delete");
+    itemContent = document.createTextNode("Delete");
     newDeleteButton.appendChild(itemContent);
-    newDeleteButton.className = last.bookId;
-    newDeleteButton.id = "remove-button";
+    newDeleteButton.className = book.bookId;
+    newDeleteButton.classList.add('remove-button');
+    libraryDisplay.appendChild(newDeleteButton); 
     newDeleteButton.addEventListener('click', function() {
-        const bookElements = Array.from(document.getElementsByClassName(last.bookId));
+        const bookElements = Array.from(document.getElementsByClassName(book.bookId));
         console.log(bookElements);
         for (let item of bookElements) {
             item.remove();
         }
-        myLibrary.splice(last.index, 1);
-        // adjustBookIndices();
+        myLibrary.splice(book.index, 1);
+        // adjustBookIndices(); here
     });
-    libraryDisplay.appendChild(newDeleteButton);
 }
 
-// function adjustBookIndices() {}
+function addStatusDropdown(book) {
+    // Implement Modify Status Dropdown
+    let statusButton = document.createElement("p");
+    let itemContent = document.createTextNode(book.status);
+    statusButton.className = book.bookId;
+    statusButton.classList.add('status', 'item');
+    statusButton.id = book.index;
+    statusButton.appendChild(itemContent);
+
+    statusButton.addEventListener('click', function(event) {
+        // event.stopImmediatePropagation();
+        statusButton.removeChild(statusButton.firstChild);
+        let container = enableDropdownOptions(statusButton, myLibrary[statusButton.id]);
+        let options = container.children;
+        for (let option of options) {
+            option.addEventListener('click', function(event) {
+                statusButton.textContent = option.textContent;
+                myLibrary[statusButton.id].status = option.textContent;
+                // event.stopPropagation();
+            });
+        }
+        statusButton.classList.add('status', 'item');
+        statusButton.appendChild(container);
+    });
+    
+    statusButton.addEventListener('mouseenter', function(event) {
+        statusButton.textContent = '';
+        const editText = document.createTextNode('Click to Edit');
+        statusButton.appendChild(editText);
+        // event.stopPropagation();
+    });
+    statusButton.addEventListener('mouseleave', function(event) {
+        const statusText = document.createTextNode(myLibrary[statusButton.id].status);
+        statusButton.removeChild(statusButton.firstChild);
+        statusButton.appendChild(statusText);
+        // event.stopPropagation();
+    });
+    
+    libraryDisplay.appendChild(statusButton);
+}
+
+function enableDropdownOptions(book) {
+    let unread = document.createElement("button");
+    unread.className = 'status-dropdown';
+    const unreadText = document.createTextNode('Unread');
+    unread.appendChild(unreadText);
+
+    let currentlyReading = document.createElement("button");
+    currentlyReading.className = 'status-dropdown';
+    const currentlyReadingText = document.createTextNode('Currently Reading');
+    currentlyReading.appendChild(currentlyReadingText);
+
+    let finishedReading = document.createElement("button");
+    finishedReading.className = 'status-dropdown';
+    const finishedReadingText = document.createTextNode('Finished Reading');
+    finishedReading.appendChild(finishedReadingText);
+
+    let dropdownContainer = document.createElement("div");
+    dropdownContainer.className = 'options';
+
+    dropdownContainer.appendChild(unread);
+    dropdownContainer.appendChild(currentlyReading);
+    dropdownContainer.appendChild(finishedReading);
+    
+    return (dropdownContainer);
+}
 
 function addBookToLibrary(entry) {
    let title = entry.elements.title.value;
